a6ba25a70689285000825e0ddbb8c8d9d9ca7bf9
Fan Pu <zengfanpu@gmail.com>
2017-03-06 15:55:09 +0800

    db.js for connections and graceful termination

--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,3 +1,6 @@
 module.exports.about = function(req, res) {
-    res.render('generic-text', { title: 'About' });
+    res.render('generic-text', {
+	title: 'About Loc8r',
+	content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\n Used worldwide in 78 different countries and trusted by millions of daily active users. Loc8 your next productivity haunt today!'
+    });
 };
