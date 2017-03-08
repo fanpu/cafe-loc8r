6efa46da26516c2d81137209115a93049f244267
Fan Pu <zengfanpu@gmail.com>
2017-03-08 11:51:29 +0800

    set credentials in env var

--- app_server/models/db.js
+++ app_server/models/db.js
@@ -1,7 +1,7 @@
 var mongoose = require( 'mongoose' );
 var dbURI = 'mongodb://localhost/Loc8r';
 if(process.env.NODE_ENV === 'production') {
-    dbURI = 'mongodb://fanpu:herokudb.ds121190.mlab.com:21190/heroku_1pq86whw';
+    dbURI = process.env.MONGOLAB_URI; //'mongodb://fanpu:herokudb.ds121190.mlab.com:21190/heroku_1pq86whw';
 }
 mongoose.connect(dbURI);
 
